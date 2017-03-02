81b3ea583547bd17d2dc6be43fd1ddf797c25c77
h_djebli <djebli@lvsc-mediterranee.com>
2017-03-02 21:17:11 +0100

    AsyncTask with Permission

--- app/src/main/java/a3ti/atelier/mobile/atelier3ti2017/MainActivity.java
+++ app/src/main/java/a3ti/atelier/mobile/atelier3ti2017/MainActivity.java
@@ -7,11 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,28 +27,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onLoadClick(View view) {
-        String urlString="http://192.168.137.1:8090/scripts/test.php";
+        /*String urlString="http://192.168.137.1:8090/scripts/test.php";
         LoadTask backgroundTask = new LoadTask();
-        backgroundTask.execute(urlString);
+        backgroundTask.execute(urlString);*/
+
+        String urlString="http://192.168.137.1/gps/test.php";
+        BackgroundTask task=new BackgroundTask();
+        task.execute(urlString);
+
+
     }
-    private class LoadTask extends AsyncTask<String,Void,String>
+
+
+    private class BackgroundTask extends AsyncTask<String,Void,String>
     {
+
         @Override
         protected String doInBackground(String... params) {
-            HttpURLConnection c=null;
+            HttpURLConnection connection = null;
+            URL url= null;
             try {
-                String urlString=params[0];
-                URL url=new URL(urlString);
-                c=(HttpURLConnection)url.openConnection();
-                c.setRequestMethod("GET");
-                c.setConnectTimeout(15000 /* milliseconds */);
-                c.setDoInput(true);
+                url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
 
-                c.connect();
-                int mStatusCode = c.getResponseCode();
-                switch (mStatusCode) {
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(15000 /* milliseconds */);
+                connection.setDoInput(true);
+                connection.connect();
+
+                int code= connection.getResponseCode();
+                switch (code)
+                {
                     case 200:
-                        BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                         StringBuilder sb = new StringBuilder();
                         String line;
                         while ((line = br.readLine()) != null) {
@@ -54,26 +68,27 @@ public class MainActivity extends AppCompatActivity {
                         br.close();
                         String result =  sb.toString();
                         return result;
+
+                    case 404:
+                        return "page non trouvée";
+
+                    default:
+                        return "Code non géré";
                 }
-                return "";
-            } catch (Exception ex) {
-                Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
-                return "Error connecting to server";
-            } finally {
-                if (c != null) {
-                    try {
-                        c.disconnect();
-                    } catch (Exception ex) {
-//                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Logger.getLogger(getClass().getName())
+                        .log(Level.SEVERE, null, e);
+                Toast.makeText(getBaseContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                return "Erreur";
             }
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            TextView textView = (TextView)findViewById(R.id.resultTextView);
+            TextView textView = (TextView) findViewById(R.id.resultTextView);
             textView.setText(s);
         }
     }
