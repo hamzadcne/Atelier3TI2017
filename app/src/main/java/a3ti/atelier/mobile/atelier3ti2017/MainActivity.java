fb21ce6b5184308a72b6c83a3b28b9e1b7c6fa4a
h_djebli <djebli@lvsc-mediterranee.com>
2017-03-06 23:01:59 +0100

    PostDataTask

--- app/src/main/java/a3ti/atelier/mobile/atelier3ti2017/MainActivity.java
+++ app/src/main/java/a3ti/atelier/mobile/atelier3ti2017/MainActivity.java
@@ -6,11 +6,13 @@ import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -20,6 +22,8 @@ import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String username;
+    private String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +34,15 @@ public class MainActivity extends AppCompatActivity {
         /*String urlString="http://192.168.137.1:8090/scripts/test.php";
         LoadTask backgroundTask = new LoadTask();
         backgroundTask.execute(urlString);*/
+        EditText usernameTxt=(EditText)findViewById(R.id.usernameTxtView);
+        EditText passwordTxt=(EditText)findViewById(R.id.passwordTxtView);
 
-        String urlString="http://192.168.137.1/gps/test.php";
-        BackgroundTask task=new BackgroundTask();
+        username=usernameTxt.getText().toString();
+        password=passwordTxt.getText().toString();
+
+        String urlString="http://192.168.137.1/test.php";
+        //BackgroundTask task=new BackgroundTask();
+        PostDataTask task = new PostDataTask();
         task.execute(urlString);
 
 
@@ -41,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
     private class BackgroundTask extends AsyncTask<String,Void,String>
     {
-
         @Override
         protected String doInBackground(String... params) {
             HttpURLConnection connection = null;
@@ -54,6 +63,65 @@ public class MainActivity extends AppCompatActivity {
                 connection.setConnectTimeout(15000 /* milliseconds */);
                 connection.setDoInput(true);
                 connection.connect();
+
+                int code= connection.getResponseCode();
+                switch (code)
+                {
+                    case 200:
+                        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            sb.append(line).append("\n");
+                        }
+                        br.close();
+                        String result =  sb.toString();
+                        return result;
+
+                    case 404:
+                        return "page non trouvée";
+
+                    default:
+                        return "Code non géré";
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Logger.getLogger(getClass().getName())
+                        .log(Level.SEVERE, null, e);
+                Toast.makeText(getBaseContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                return "Erreur";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            TextView textView = (TextView) findViewById(R.id.resultTextView);
+            textView.setText(s);
+        }
+    }
+    private class PostDataTask extends AsyncTask<String,Void,String>
+    {
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            URL url= null;
+            try {
+                url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(15000 /* milliseconds */);
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                String s = "username="+username+"&password=" + password;
+
+                connection.setFixedLengthStreamingMode(s.getBytes().length);
+                PrintWriter out = new PrintWriter(connection.getOutputStream());
+                out.print(s);
+                out.close();
 
                 int code= connection.getResponseCode();
                 switch (code)
