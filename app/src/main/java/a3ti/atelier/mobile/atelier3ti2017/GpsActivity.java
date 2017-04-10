a0244460ddb30ed1c933ff354f11fde74539d98a
h_djebli <djebli@lvsc-mediterranee.com>
2017-04-10 10:10:08 +0100

    Android 6 Permission

--- app/src/main/java/a3ti/atelier/mobile/atelier3ti2017/GpsActivity.java
+++ app/src/main/java/a3ti/atelier/mobile/atelier3ti2017/GpsActivity.java
@@ -7,7 +7,9 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.provider.SyncStateContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,12 +26,40 @@ public class GpsActivity extends AppCompatActivity {
     LocationManager manager;
     LocationListener locationChangedListener;
     Helpers.Geocoder mGeoCoder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
         mGeoCoder = new Helpers.Geocoder();
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 99);
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        getLocationupdates();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 99 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            getLocationupdates();
+    }
+
+    private void getLocationupdates() {
         locationChangedListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
@@ -42,12 +72,12 @@ public class GpsActivity extends AppCompatActivity {
                 TextView lonTxtView = (TextView) findViewById(R.id.longitude_value);
                 lonTxtView.setText(String.valueOf(lon));
 
-                mGeoCoder.ReverseGeocode(lat,lon,15,new Callback(){
+                mGeoCoder.ReverseGeocode(lat, lon, 15, new Callback() {
                     @Override
                     public void onFinished(String result) {
                         super.onFinished(result);
                         try {
-                            NominatimResult nominatimResult = new Gson().fromJson(result,NominatimResult.class);
+                            NominatimResult nominatimResult = new Gson().fromJson(result, NominatimResult.class);
                             TextView addressTxtView = (TextView) findViewById(R.id.address_value);
                             addressTxtView.setText(nominatimResult.display_name);
                         } catch (JsonSyntaxException e) {
@@ -55,18 +85,6 @@ public class GpsActivity extends AppCompatActivity {
                         }
                     }
                 });
-
-                if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
             }
 
             @Override
@@ -84,13 +102,19 @@ public class GpsActivity extends AppCompatActivity {
 
             }
         };
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationChangedListener);
-
-
     }
-
     public void onShareClick(View view) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
